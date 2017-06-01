269d92408c4664db697d9539cefa6272b9ce29db
glassfan <dakotaglass@outlook.com>
2017-06-01 10:19:07 -0700

    Add files via upload

--- pull_urls.py
+++ pull_urls.py
@@ -6,26 +6,33 @@ import cv2
 import numpy as np
 import os
 
-def pull_images():
-    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n03593526'   #replace the wnid="<insert synset #>"
-    neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
-    pic_num = 1314  # hardcoded placeholder, for appending further images
+def pull_samples(url_link, filename, url_num, height, width):
+    images_link = url_link   #replace the wnid="<insert synset #>"
+    image_urls = urllib.request.urlopen(images_link).read().decode()
+    pic_num = url_num  # hardcoded placeholder, for appending further images
     
-    for i in neg_image_urls.split('\n'):
+    for i in image_urls.split('\n'):
         try:
             print(i)
-            urllib.request.urlretrieve(i, "resized_negatives/"+str(pic_num)+".jpg")
-            img = cv2.imread("resized_negatives/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE) #relative path to destination folder
-            # should be larger than postive sample images
-            resized_image = cv2.resize(img, (100, 100))
-            cv2.imwrite("resized_negatives/"+str(pic_num)+".jpg",resized_image)
+            urllib.request.urlretrieve(i, filename+str(pic_num)+".jpg")
+            img = cv2.imread(filename+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE) #relative path to destination folder
+            # negative samples should be larger than postive sample images
+            '''
+            #to preserve aspect ratio (use if needed)
+            r = 100.0 / img.shape[1]
+            dim = (height, int(img.shape[0] * r))
+            resized_image = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+            '''
+            resized_image = cv2.resize(img, (height, width))
+            cv2.imwrite(filename+str(pic_num)+".jpg",resized_image)
             pic_num += 1
             
         except Exception as e:
-            print(str(e)) 
-
-def find_errors():
-    for file_type in ['resized_negatives']:
+            print(str(e))
+            
+            
+def find_errors(filename):
+    for file_type in [filename]:
         for img in os.listdir(file_type):
             for badPic in os.listdir('rejects'): #'rejects' is src folder of error jpgs copy and pasted from dst folder of pulled images
                 try:
@@ -40,5 +47,6 @@ def find_errors():
                     print(str(e))
 
 
-#pull_images()
-find_errors()
+
+pull_samples("http://image-net.org/api/text/imagenet.synset.geturls?wnid=n04255586", "resized_positives/", 1, 75, 75)
+find_errors("resized_positives/")
