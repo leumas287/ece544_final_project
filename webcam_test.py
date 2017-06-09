b42a7b4691b84fa7660ea915c6d71dba69fc4816
glassfan <dakotaglass@outlook.com>
2017-06-09 16:17:19 -0700

    Add files via upload

--- webcam_test.py
+++ webcam_test.py
@@ -1,64 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  5 22:20:23 2017
-
-@author: Dakota
+# Created on Mon Jun  5 22:20:23 2017
+# webcam_test.py
+# @author: Dakota
+# USAGE: Requires:  python 3+
+#                   Opencv 3.2 ["import cv2"]
+#                   usb web camera 
+#                  
+# Description:
+# Python script runs in a main loop capturing frames from webcamera.
+# Used to test the functionality of the web camera
+# 
+# USAGE:
+    plug in USB webcamera, run script
+    *Depending on configuration Line # 22 cv2.VideoCapture(0) needs to be changed
+    * cv2.VideoCapture(<Insert Correct value here>)
 """
 
-import numpy as np
 
 import cv2
 
-
-
-
-
-# In[ ]:
-
-
-
 cap = cv2.VideoCapture(0)
-
-
-
-
-
-# In[ ]:
-
-
 
 while(True):
 
     # Capture frame-by-frame
 
     ret, frame = cap.read()
-
-
-
-    # Our operations on the frame come here
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
-
     # Display the resulting frame
-
     cv2.imshow('frame',gray)
-
+    
+    # break if q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
-
         break
-
-
-
-
-
-# In[ ]:
-
-
-
 # When everything done, release the capture
-
 cap.release()
-
 cv2.destroyAllWindows()
